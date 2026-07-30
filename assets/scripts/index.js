2956d5b39cabd37bd7d5b504073296bcaee4477b
lastranger21 <labib.cerdas@gmail.com>
2026-07-30 17:06:50 +0700

    add: optimized layout and filling data dummy

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,11 +1,47 @@
 let dataDummy =[{
     name: "John Doe",
-    class: "10th Grade",
+    class: "10",
     score: 85
 },{
     name: "Jane Smith",
-    class: "10th Grade",
+    class: "10",
     score: 92
+},{
+    name: "Michael Johnson",
+    class: "11",
+    score: 78
+},{
+    name: "Emily Davis",
+    class: "11",
+    score: 88
+},{
+    name: "William Brown",
+    class: "10",
+    score: 95
+},{
+    name: "Olivia Wilson",
+    class: "12",
+    score: 81
+},{
+    name: "Oliver Martinez",
+    class: "12",
+    score: 94
+}, {
+    name: "Kylian Mbappe",
+    class: "10",
+    score: 96
+},{
+    name: "Lionel Messi",
+    class: "12",
+    score: 87
+},{
+    name: "Cristiano Ronaldo",
+    class: "12",
+    score: 86
+},{
+    name: "Neymar Jr.",
+    class: "11",
+    score: 89   
 }]
 
 function avgScore(data){
@@ -24,9 +60,10 @@ document.getElementById("table-body").innerHTML = dataDummy.map((item, index) =>
 document.getElementById("search-input").addEventListener("input", function(event) {
     event.preventDefault();
     const name = document.getElementById("search-input").value;
-    document.getElementById("table-body").innerHTML = dataDummy.filter((item) => {
+    const filtered = dataDummy.filter((item) => {
         return item.name.toLowerCase().includes(name.toLowerCase());
-    }).map((item, index) => {
+    })
+    document.getElementById("table-body").innerHTML = filtered.map((item, index) => {
         return `<tr>
                     <th scope="row">${index + 1}</th>
                     <td>${item.name}</td>
@@ -34,6 +71,7 @@ document.getElementById("search-input").addEventListener("input", function(event
                     <td>${item.score}</td>
             </tr>`
 }).join("")
+    document.getElementById("avg-score").textContent = avgScore(filtered).toFixed(2);
 })
 avgScore(dataDummy)
 document.getElementById("avg-score").textContent = avgScore(dataDummy).toFixed(2);
